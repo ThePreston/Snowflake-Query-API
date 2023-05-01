242cae80def6645c710b047339abad6fc9a876d0
ThePreston <84995595+ThePreston@users.noreply.github.com>
2023-05-01 13:39:41 -0500

    Create variables.tf

---
+++ IaC/variables.tf
@@ -0,0 +1,12 @@
+
+variable "location" {
+  description = "Location of resource"
+  type        = string
+  default     = "Central US"
+}
+
+variable "projectnamingconvention" {
+  description = "Naming Convention"
+  type        = string
+  default     = "tf-default"
+}
